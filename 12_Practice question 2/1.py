16dd2d0c0e114d351be62eba460a9297e0fc94fb
Nishant141008 <nnishant141008@gmail.com>
2026-07-16 14:14:14 +0530

    second commit

---
+++ 12_Practice question 2/1.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+print(num)
+
+if num < 0:
+  print("The number is negative!")
+elif  num > 0:
+  print("The number is positive!") 
+else:
+  print("The number is Zero!")0
